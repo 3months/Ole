8e479bdb3492d6678ee5563cc51eaa569d625baf
Josh McArthur <joshua.mcarthur@gmail.com>
2011-10-10 14:44:51 +1300

    Add support for Ole'ing from Github

--- ole.rb
+++ ole.rb
@@ -1,7 +1,24 @@
 class Ole < Sinatra::Base
   
   get '/ole' do
+    ole
+  end
+  
+  get '/ole/github' do
+    payload = JSON.parse(params[:payload])
+    last_commit = payload.commits.last.message
+    repository = payload.repository.name
+    ole_if_valid(repository, last_commit)
+  end
+  
+  private
+  
+  def ole
     system("cvlc #{File.join(File.dirname(__FILE__), 'assets', 'ole_trumpet.mp3')} --play-and-exit")
   end
   
+  def ole_if_valid(repo, msg)
+    return unless ENV['ole_repos'].split(',').include?(repo) || msg =~ /\AOLE\:/
+    ole
+  end
 end
